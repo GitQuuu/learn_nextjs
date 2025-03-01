bd716ead55be43860c9cf907f8ee0d544253dc28
Qu <qu@peer-performance.com>
2025-03-01 23:28:29 +0100

    using the new component

--- react-crash-course/src/components/postList.jsx
+++ react-crash-course/src/components/postList.jsx
@@ -1,12 +1,16 @@
 import Post from "@/components/post.jsx";
+import NewPost from "@/components/NewPost.jsx";
 
 export default function PostList(){
     return (
-        <div className='flex'>
-            <Post author="Qu" body="React.js is okay"></Post>
-            <Post author="Qu" body="Still fan of Angular"></Post>
-            <Post author="Qu" body="Maybe Svelte 5"></Post>
-        </div>
+        <>
+            <NewPost></NewPost>
+            <div className='flex'>
+                <Post author="Qu" body="React.js is okay"></Post>
+                <Post author="Qu" body="Still fan of Angular"></Post>
+                <Post author="Qu" body="Maybe Svelte 5"></Post>
+            </div>
+        </>
     )
 }
 
