b2ccc3f37f48d7c77af8294a5b9cfa5759170f9b
Qu <qu@peer-performance.com>
2025-03-02 13:34:37 +0100

    minor fix to styling

--- react-crash-course/src/components/Modal.jsx
+++ react-crash-course/src/components/Modal.jsx
@@ -4,7 +4,7 @@ export function Modal({children}) {
     return (
         <>
             <div className={classes.backdrop}/>
-            <dialog open className={'flex mx-auto'}>
+            <dialog open className={classes.modal + ' flex mx-auto'}>
                 {children}
             </dialog>
         </>
