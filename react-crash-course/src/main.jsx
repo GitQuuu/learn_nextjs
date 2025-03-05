6fe9499d6b71865ecc1065c9c11e1b035e738717
Qu <qu@peer-performance.com>
2025-03-05 23:55:17 +0100

    Configure the default route

--- react-crash-course/src/main.jsx
+++ react-crash-course/src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import {RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Home} from "lucide-react";
+import App from "@/App.jsx";
+
+const router = createBrowserRouter([
+    {path: '/', component: <App/>},
+    ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider/>
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
