fa8d34bf67638ffd6104fb2b97f285028e4cbf08
Qu <qu@peer-performance.com>
2025-03-06 00:21:35 +0100

    41.Working with layout routes

---
+++ react-crash-course/src/components/routes/RootLayout.jsx
@@ -0,0 +1,8 @@
+import MainHeader from "@/components/MainHeader.jsx";
+
+function RootLayout() {
+    return <>
+    <MainHeader />
+    </>
+}
+export default RootLayout;
--- react-crash-course/src/main.jsx
+++ react-crash-course/src/main.jsx
@@ -1,17 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import App from "@/App.jsx";
 import NewPost from "@/components/NewPost.jsx";
+import RootLayout from "@/components/routes/RootLayout.jsx";
 
 const router = createBrowserRouter([
-    {path: '/', element: <App/>},
-    {path: '/create-post', element: <NewPost/>},
-    ])
+    {
+        path: '/', element: <RootLayout/>, children:
+            [
+                {path: '/', element: <App/>},
+                {path: '/create-post', element: <NewPost/>},
+            ]
+    },
+
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router}/>
+    </StrictMode>,
 )
