83560699cb3a2280cb55d86094cf1ec6fcb5fbdd
Qu <DevQu@hotmail.com>
2025-03-06 10:03:44 +0100

    use navigate hook to close the Modal programmatically

--- react-crash-course/src/components/Modal.jsx
+++ react-crash-course/src/components/Modal.jsx
@@ -1,9 +1,16 @@
 import classes from './Modal.module.css';
+import {useNavigate} from "react-router-dom";
 
-export function Modal({children, onOutsideModalClick}) {
+export function Modal({children}) {
+
+    const navigate = useNavigate();
+
+    function closeHandler(){
+     navigate("..");
+    }
     return (
         <>
-            <div className={classes.backdrop} onClick={onOutsideModalClick}/>
+            <div className={classes.backdrop} onClick={closeHandler}/>
             <dialog open className={classes.modal + ' flex mx-auto'}>
                 {children}
             </dialog>
