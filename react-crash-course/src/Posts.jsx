fc38de94c12c9a99feaa3c0087bd118c5e12d0c2
Qu <Qu@peer-performance.com>
2025-03-06 08:02:41 +0100

    Clean up the posts component for function no longer in use

--- react-crash-course/src/Posts.jsx
+++ react-crash-course/src/Posts.jsx
@@ -1,23 +1,10 @@
 import './App.css'
 import PostList from "@/components/postList.jsx";
-import MainHeader from "@/components/MainHeader.jsx";
-import {useState} from "react";
 
 function Posts() {
-
-    const [modalIsVisible, setModalIsVisible] = useState(false);
-
-    function showModal() {
-        setModalIsVisible(true);
-    }
-
-    function hideModal() {
-        setModalIsVisible(false);
-    }
-
     return (
         <>
-            <PostList onCreatePostClickedShowModal={modalIsVisible} onOutsideModalClicked={hideModal}></PostList>
+            <PostList></PostList>
         </>
     )
 }
