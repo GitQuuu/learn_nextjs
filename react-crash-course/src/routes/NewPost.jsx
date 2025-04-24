aa7558b7e97799b078a0b408f80c6daf7c412e87
Qu <qu@peer-performance.com>
2025-04-24 11:19:20 +0200

    Handles form submission for new posts
    The action function now correctly extracts form data from the request and prepares it for submission to the backend.
    
    It extracts the form data and converts it into a JavaScript object, which is then used to populate the body of the POST request.

--- react-crash-course/src/routes/NewPost.jsx
+++ react-crash-course/src/routes/NewPost.jsx
@@ -30,7 +30,10 @@ function NewPost() {
 }
 
 export default NewPost;
-export function action(){
+export async function action({request}){
+    const formData = await request.formData();
+    const postData = Object.fromEntries(formData);
+
     fetch("http://localhost:8080/posts", {
         method: "POST",
         body: JSON.stringify(postData),
