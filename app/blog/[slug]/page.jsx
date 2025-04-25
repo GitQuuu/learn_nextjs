751214010c81c519b101a09d0e35199a334d7030
Qu <DevQu@hotmail.com>
2025-04-25 10:59:58 +0200

    95. Init Foodie App

--- app/blog/[slug]/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function BlogPostPage({params}) {
-    console.log(params)
-    return (
-        <main>
-            <h1>Blog Post</h1>
-            <h4>{params.slug}</h4>
-        </main>
-    )
-}
