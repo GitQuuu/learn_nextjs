e268dae203682d27722f3418e34bcd9d5d70e9a4
Qu <DevQu@hotmail.com>
2025-04-28 13:48:18 +0200

    107.Adds basic layout for the meals page
    Implements a basic header and main section for the meals page.
    
    The header includes a title, a subtitle, and a link to share a new recipe.
    The main section will be used to display the meals.

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,7 +1,21 @@
+import classes from './page.module.css';
+import Link from "next/link";
+
 export default function Meals () {
     return (
-        <main>
-            <h1>Meals</h1>
-        </main>
+       <>
+       <header className={classes.header}>
+           <h1>Delicious meals created{' '}
+               <span className={classes.highlight}>by you</span>
+           </h1>
+           <p1>Choose your favorite recipe and cook it yourself</p1>
+           <p className={classes.cta}>
+               <Link href="/meals/share" >Share your favorite recipe</Link>
+           </p>
+       </header>
+           <main className={classes.main}>
+
+           </main>
+       </>
     )
 }
