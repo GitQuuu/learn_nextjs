352b99ac73cf44071bb49b82850959117c9e98a9
Qu <DevQu@hotmail.com>
2025-04-28 10:05:47 +0200

    Restructures main header component
    Moves main header related components into a dedicated directory
    for better organization and maintainability.
    
    Also, updates the logo link destination.

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -2,13 +2,13 @@ import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
-import MainHeaderBackground from "@/components/main-header-background";
+import MainHeaderBackground from "@/components/main-header/main-header-background";
 
 export default function MainHeader() {
     return <>
         <MainHeaderBackground></MainHeaderBackground>
         <header className={classes.header}>
-            <Link className={classes.logo} href="/">
+            <Link className={classes.logo} href="/public">
                 <Image src={logoImg} alt="A plate with food on it" />
                 NextLevel Food
             </Link>
