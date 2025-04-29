5f12300c94bea54b2d00f08344960e21029ab7c0
Qu <DevQu@hotmail.com>
2025-04-29 10:25:16 +0200

    114. Loading & rendering meal details via dynamic route
    Implements the meal details page with styling.
    
    The page displays the meal's image, title, creator, summary, and instructions.

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -1,7 +1,28 @@
+import classes from './page.module.css'
+import Image from 'next/image';
+
 export default function MealDetails({params}) {
     return (
-        <main>
-            <h1>Meal Slug</h1>
-        </main>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={image} alt={title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>TITLE</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${'EMAIL'}`}>NAME</a>
+                    </p>
+                    <p className={classes.summary}>SUMMARY</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html:'...'
+                }}>
+                    
+                </p>
+            </main>
+        </>
     )
 }
