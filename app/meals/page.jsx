b56db6aca88a86e4cb975cda0d0cbcc8f8bdb10f
Qu <DevQu@hotmail.com>
2025-04-29 09:03:22 +0200

    111.Improves meal loading with Suspense
    Introduces Suspense to handle asynchronous meal loading, enhancing the user experience by displaying a fallback message while data is being fetched.
    
    Moves the loading component to the meals directory.

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -2,9 +2,15 @@ import classes from './page.module.css';
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import {getMeals} from "@/lib/meals";
+import {Suspense} from "react";
 
-export default async function Meals () {
+async function Meals() {
     const meals = await getMeals();
+    return <MealsGrid meals={meals}></MealsGrid>
+}
+
+export default function MealsPage () {
+
     return (
        <>
        <header className={classes.header}>
@@ -17,7 +23,10 @@ export default async function Meals () {
            </p>
        </header>
            <main className={classes.main}>
-               <MealsGrid meals={meals}></MealsGrid>
+               <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                   <Meals></Meals>
+               </Suspense>
+
            </main>
        </>
     )
