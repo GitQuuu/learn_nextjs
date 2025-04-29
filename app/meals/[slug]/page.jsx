857d200c69a4a670506d5c2768dcae0028e78b5a
Qu <DevQu@hotmail.com>
2025-04-29 13:07:57 +0200

    115.Adds not found page for meals
    Adds a not found page to handle cases where a meal cannot be found, improving the user experience by providing a helpful message instead of a generic error.

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -1,9 +1,15 @@
 import classes from './page.module.css'
 import Image from 'next/image';
 import {getMeal} from "@/lib/meals";
+import {notFound} from "next/navigation";
 
 export default async function MealDetails({params}) {
     const meal = await getMeal(params.slug)
+
+    if (!meal) {
+        notFound();
+    }
+
     meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
     return (
         <>
