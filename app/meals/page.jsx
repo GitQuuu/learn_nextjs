0a57d2341e3988f2c5729f84573add02d1e7708c
Qu <qu@peer-performance.com>
2025-04-30 23:43:25 +0200

    Improves meal creation and storage
    Updates the meal creation process to handle image uploads correctly.
    
    It ensures proper file handling and storage in the public directory.
    The changes also add input validation to prevent errors.

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,8 +1,9 @@
 import classes from './page.module.css';
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
-import {getMeals} from "@/lib/meals";
+
 import {Suspense} from "react";
+import {getMeals} from "@/lib/meals";
 
 async function Meals() {
     const meals = await getMeals();
