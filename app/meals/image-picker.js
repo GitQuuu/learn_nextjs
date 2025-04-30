c251eb103d070c21d6eee0638a745aac2ae61558
Qu <qu@peer-performance.com>
2025-04-30 11:34:02 +0200

    118.Adds image preview before meal submission
    Implements image preview functionality in the image picker component.
    This allows users to see the selected image before submitting the meal form.
    
    Adds state to store the picked image data as a data URL.
    Updates the component to display a preview of the selected image.
    Adds a file reader to convert image to data URL

--- app/meals/image-picker.js
+++ app/meals/image-picker.js
@@ -1,17 +1,36 @@
 'use client';
 import classes from './image-picker.module.css'
-import {useRef} from "react";
+import {useRef, useState} from "react";
+import Image from "next/image";
 
 export default function ImagePicker({label, name}) {
+    const [pickedImage, setPickedImage] = useState()
     const imageInput = useRef(null);
 
     function handleImagePick() {
      imageInput.current.click();
     }
 
+    function handleImageChange(e) {
+        const file = e.target.files[0];
+
+        if (!file) {
+           return;
+        }
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+           setPickedImage(fileReader.result);
+        }
+        fileReader.readAsDataURL(file);
+    }
+
     return <div className={classes.picker}>
         <label htmlFor={name}>{label}</label>
         <div className={classes.controls}>
+            <div className={classes.preview}>
+                {!pickedImage && <p>No image picked yet</p>}
+                {pickedImage && <Image src={pickedImage} alt="Meal image selected by user" fill />}
+            </div>
             <input
                 className={classes.input}
                 type="file"
@@ -20,6 +39,7 @@ export default function ImagePicker({label, name}) {
                 image/jpeg"
                 name={name}
                 ref={imageInput}
+                onChange={handleImageChange}
             />
             <button
                 className={classes.button}
