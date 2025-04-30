86698795f490c770bacd966d62f99eaa8c57826a
Qu <qu@peer-performance.com>
2025-04-30 22:06:58 +0200

    Connects share meal form to action
    Connects the share meal form submission to the server action for processing.
    Also, updates the image path to store images in the /images directory.

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,5 +1,6 @@
 import classes from './page.module.css';
 import ImagePicker from "@/app/meals/image-picker";
+import {shareMeal} from "@/lib/action";
 
 export default  function ShareMealPage() {
 
@@ -12,7 +13,7 @@ export default  function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form} action={handleSubmit}>
+                <form className={classes.form} action={shareMeal}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
