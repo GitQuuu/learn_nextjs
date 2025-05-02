5c9a58bd1ec0824c01de0487f82543ae63073ad9
Qu <DevQu@hotmail.com>
2025-05-02 13:33:59 +0200

    139.Refactors news page to use dummy data
    Moves the news items data to a separate file for better organization and maintainability.
    Updates the news page component to fetch and display news articles with images from the dummy data.
    Adds a div wrapper to the RootLayout

--- app/layout.js
+++ app/layout.js
@@ -2,17 +2,19 @@ import './globals.css';
 import MainHeader from './components/main-header/main-header';
 
 export const metadata = {
-  title: 'Next.js Page Routing & Rendering',
-  description: 'Learn how to route to different pages.',
+    title: 'Next.js Page Routing & Rendering',
+    description: 'Learn how to route to different pages.',
 }
 
-export default function RootLayout({ children }) {
- return (
-    <html lang="en">
-      <body>
-      <MainHeader/>
-      {children}
-      </body>
-    </html>
-  )
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+        <body>
+        <div id="page">
+            <MainHeader/>
+            {children}
+        </div>
+        </body>
+        </html>
+    )
 }
