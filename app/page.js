23285b026e4a00d734f2aaf14158b4a6112b4431
Qu <DevQu@hotmail.com>
2025-05-02 13:03:58 +0200

    139.Populates news page with hardcoded data
    Populates the news page with initial hardcoded news data.
    Adds descriptive attributes such as title, image, and content.
    Refactors main header to have a logo and navigation.
    Removes the home link.

--- app/page.js
+++ app/page.js
@@ -4,9 +4,6 @@ export default function HomePage() {
     return (
         <div id="home">
             <h1>Next.js Routing & Page Rendering</h1>
-            <div>
-                <Link href="/news"></Link>News
-            </div>
         </div>
     );
 }
