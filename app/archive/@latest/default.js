4207c8ce416fff120da22bc68866cfdc8c1efd27
Qu <qu@peer-performance.com>
2025-05-05 23:04:09 +0200

    Replaces archive page with latest news page
    Replaces the generic archive page with a dedicated latest news page.
    
    Improves user experience by directly showcasing the latest news content.

---
+++ app/archive/@latest/default.js
@@ -0,0 +1,12 @@
+import {getLatestNews} from "@/app/lib/news";
+import NewsList from "@/app/components/news-list/news-list";
+
+export default function LatestNewsPage() {
+    const latestNews = getLatestNews();
+    return(
+        <>
+            <h2>Latest news</h2>
+            <NewsList news={latestNews}></NewsList>
+        </>
+    )
+}
--- app/archive/@latest/page.js
+++
@@ -1,7 +0,0 @@
-export default function ArchivePage() {
-    return (
-        <div>
-            <h1>Latest page</h1>
-        </div>
-    );
-}
