d89968b0d9f45996c0b9bfd37c81fe54ed6e7536
Qu <qu@peer-performance.com>
2025-05-05 21:43:31 +0200

    141. Parallel routes

---
+++ app/archive/layout.js
@@ -0,0 +1,11 @@
+export default function Layout({ archive, latest }) {
+    return <div>
+        <h1>news archive</h1>
+        <section id="archive-filter">
+            {archive}
+        </section>
+        <section id="archive-latest">
+            {latest}
+        </section>
+    </div>
+}
