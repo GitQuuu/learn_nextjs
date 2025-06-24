f3c9e835130075460aa4f5ff78f1e04e1f11e945
Qu <Quanvle@live.dk>
2025-06-24 11:30:30 +0200

    on root redirect to signin

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
+import {redirect} from "next/navigation";
+
 export default function Home() {
   return (
-    <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
-    </main>
+   redirect("/signin")
   );
 }
