713001eacebd66240b3765a094b766b33ac62f26
Qu <Quanvle@live.dk>
2025-06-24 13:35:27 +0200

    Implements authentication for the homepage
    https://clerk.com/docs/references/nextjs/clerk-middleware
    Adds authentication to the homepage, redirecting users to the sign-in page if they are not authenticated. This ensures that only authenticated users can access the main content of the application.
    Also configures middleware to protect non-public routes.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,19 @@
+import {RedirectToSignIn, SignedIn, SignedOut} from "@clerk/nextjs";
+
 export default function Home() {
+
     return (
-        <main>
-            <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-            <h1>Welcome to this NextJS Course!</h1>
-            <p>🔥 Let&apos;s get started! 🔥</p>
-        </main>
+        <>
+            <SignedIn>
+                <main>
+                    <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+                    <h1>Welcome to this NextJS Course!</h1>
+                    <p>🔥 Let&apos;s get started! 🔥</p>
+                </main>
+            </SignedIn>
+            <SignedOut>
+                <RedirectToSignIn />
+            </SignedOut>
+        </>
     );
 }
